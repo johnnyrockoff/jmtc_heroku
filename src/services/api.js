9e1bcd4239d87a8d90d54319332db2ed205b1d06
Joao Paulo Rocha <johnny.rocha@pearson.com>
2022-03-01 13:02:39 -0300

    Update api.js
    Update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://jmts.herokuapp.com';
 
 export async function getAllChars() {
     try{
